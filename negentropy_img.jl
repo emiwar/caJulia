3ab064d88356d40576e03b092990ad225354e69d
Emil Wärnberg <emilwa@kth.se>
2022-10-27 12:42:41 +0200

    Using negentropy for tuning OASIS parameters and maye init image.

---
+++ negentropy_img.jl
@@ -0,0 +1,21 @@
+function negentropy_img(Y)
+    pows = mapreduce(y->(y, y^2, y^3, y^4), .+, Y;
+                     init=(0.0, 0.0, 0.0, 0.0), dims=2)
+    map(yp->negentropy_approx((yp ./ size(Y,2))...), pows)
+end
+
+function negentropy_approx(x1, x2, x3, x4)
+    var = x2 - x1^2
+    third_moment = x3 - 3*x1*x2 + 2*(x1^3)
+    forth_moment = x4 - 4*x1*x3 + 6*(x1^2)*x2 - 3*(x1^4)
+    skew2 = (third_moment^2) / (var^3)
+    kurtosis = forth_moment / (var^2) - 3.0
+    negentropy = skew2 / 12.0 + kurtosis / 48.0
+end
+
+function negentropy_approx_no_mean(Y)
+    N = size(Y,2)
+    y2, y3, y4 = (mapreduce(y->y^i, +, Y; dims=2) ./ N for i=2:4)
+    @. (y3^2 / y2^3)/12.0 + y4/(y2^2) / 48.0
+end
+
--- oasis.jl
+++ oasis.jl
@@ -1,3 +1,6 @@
+import Statistics
+import DSP
+
 struct Pool
     v::Float64
     w::Float64
@@ -43,15 +46,17 @@ function sweep_lambda(y, c, s, lambdas, gamma)
     kur = Float64[]
     c_cor = Float64[]
     s_cor = Float64[]
+    negentropy = Float64[]
     for lambda in lambdas
         c_est, s_est = oasis(y, gamma, lambda)
         push!(res, sum((c_est .- y).^2))
         push!(err, sum((c_est .- c).^2))
         push!(kur, kurtosis(c_est .- y))
+        push!(negentropy, negentropy_est(c_est .- y))
         push!(c_cor, Statistics.cor(c_est, c))
         push!(s_cor, Statistics.cor(s_est, s))
     end
-    return res, err, kur, c_cor, s_cor
+    return res, err, kur, negentropy, c_cor, s_cor
 end
 
 function kurtosis(x)
@@ -60,30 +65,88 @@ function kurtosis(x)
     return sum((x .- x_m).^4) ./ N ./ ((sum((x .- x_m).^2)./N).^2)
 end
 
-function est_sigma(y)
-    P = welch_pgram(y)
-    f = freq(P)
-    p = power(P)
-    sqrt(mean(p[f .> 0.25]) / 2)
+function negentropy_est(x)
+    N = length(x)
+    x_m = sum(x) / N
+    sd = sqrt((sum((x .- x_m).^2)./N))
+    return (sum(((x .- x_m)./sd).^3) ./ N)^2/12 + (kurtosis(x)-3)^2/48
 end
 
+function est_sigma(y)
+    P = DSP.welch_pgram(y)
+    f = DSP.freq(P)
+    p = DSP.power(P)
+    sqrt(Statistics.mean(p[f .> 0.25]) / 2)
+end
+
+function joint_opt(y, lambdas, gammas, c_true, s_true)
+    negent = zeros(length(lambdas), length(gammas))
+    c_cor = zeros(length(lambdas), length(gammas))
+    s_cor = zeros(length(lambdas), length(gammas))
+    @showprogress "Running OASIS" for i=1:length(lambdas)
+        for j=1:length(gammas)
+            lambda = lambdas[i]
+            gamma = gammas[j]
+            c_est, s_est = oasis(y, gamma, lambda)
+            negent[i, j] = negentropy_est(c_est .- y)
+            c_cor[i, j] = Statistics.cor(c_est, c_true)
+            s_cor[i, j] = Statistics.cor(s_est, s_true)
+        end
+    end
+    return negent, c_cor, s_cor
+end
+   
 T = 25000
 gamma = 0.9
-sigma = 100*0.5
-fake_s = 100 .* (randn(T).^2) .* (rand(T) .> 0.95)
+sigma = 10*1.0#0.2
+fake_s = 10 .* (randn(T).^4) .* (rand(T) .> 0.98)
 fake_c = similar(fake_s)
 fake_c[1] = fake_s[1]
 for i=2:T
     fake_c[i] = gamma*fake_c[i-1] + fake_s[i]
 end
 fake_y = fake_c + sigma*randn(T);
-lambdas = (0:500)
-res, err, kur, c_cor, s_cor = sweep_lambda(fake_y, fake_c, fake_s, lambdas, gamma)
+lambdas = (0:1000)/2
+res, err, kur, negent, c_cor, s_cor = sweep_lambda(fake_y, fake_c, fake_s, lambdas, gamma)
 ested_sigma = est_sigma(fake_y)
 plot(lambdas, sqrt.(res ./ T), label="Residual")
 plot!([lambdas[1], lambdas[end]], [sigma, sigma], label="True sigma")
 plot!([lambdas[1], lambdas[end]], [ested_sigma, ested_sigma], label="Estimated sigma")
 plot!(lambdas, sqrt.(err ./ T), label="Actual error")
-plot!(lambdas, 100*(kur .- 3), label="Kurtosis")
+plot!(lambdas, negent*10000, label="Negentropy")
+p=plot!(lambdas, 100*(kur .- 3), label="Kurtosis", ylim=(-20, 50))
+display(p)
 plot(lambdas, c_cor, label="Correlation true C vs estimated C")
 plot!(lambdas, s_cor, label="Correlation true S vs estimated S")
+
+
+lambdas = 0:200
+gammas = 0.8:0.0025:0.95
+negent, c_cor, s_cor = joint_opt(fake_y, lambdas, gammas, fake_c, fake_s);
+heatmap(gammas, lambdas, log10.(negent))
+heatmap(gammas, lambdas, log10.(1.0 .- c_cor))
+heatmap(gammas, lambdas, log10.(1.0 .- s_cor))
+
+
+loss(x) = negentropy_est(oasis(fake_y, x[1], x[2])[1] .- fake_y)
+sol = Optim.optimize(loss, [0.89, 50.0])
+sol.minimizer
+
+import HDF5
+real_c = HDF5.h5open("example_traces.h5", "r") do fid
+    HDF5.read(fid, "just_init")
+end
+ex_c = real_c[:,32]
+real_gamma= 0.88#0.9#0.85#0.88
+lambdas = (0:1000)/10
+res, err, kur, negent, c_cor, s_cor = sweep_lambda(ex_c, ex_c, ex_c, lambdas, real_gamma)
+ested_sigma = est_sigma(ex_c)
+
+p1 = plot(lambdas, sqrt.(res ./ T), label="Residual")
+#plot!([lambdas[1], lambdas[end]], [sigma, sigma], label="True sigma")
+plot!(p1, [lambdas[1], lambdas[end]], [ested_sigma, ested_sigma], label="Estimated sigma")
+#plot!(lambdas, sqrt.(err ./ T), label="Actual error")
+p2 = plot(lambdas, negent*10000, label="Negentropy")
+p3 = plot(lambdas, kur, label="Kurtosis")
+plot!(p3, [extrema(lambdas)...], [3, 3], label="3")
+display(plot(p1,p2,p3, layout=(3,1)))
